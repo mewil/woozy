46f7b1d6eacf3394bd9b9ead59731d0d18d2a0e6
Sahil Patel <sletap@umich.edu>
2020-04-07 00:28:36 -0500

    Fix compile error

--- server/db/user.js
+++ server/db/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new Schema({
-    user_id: String,
-    password: String,
-    trusted_friend_id: String,
-    avoiding_id: [String]
+const userSchema = new mongoose.Schema({
+  user_id: String,
+  password: String,
+  trusted_friend_id: String,
+  avoiding_id: [String],
 });
 
 mongoose.model('User', userSchema);
