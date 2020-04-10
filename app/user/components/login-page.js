6d607436196132ae1d7f5f5d575c46b6b1ac1a28
sydbodz <sydbodz@umich.edu>
2020-04-09 23:51:23 -0400

    Create input fields and login button

--- app/user/components/login-page.js
+++ app/user/components/login-page.js
@@ -1,10 +1,66 @@
 import { Component } from 'react';
-import { div } from 'react-hyperscript-helpers';
+import { input, h } from 'react-hyperscript-helpers';
+import styled from 'styled-components';
+
+import { Button } from '@woozy/ui';
 // import { connect } from "react-redux";
 
+const InputContainer = styled.div`
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  input {
+    max-width: 400px;
+    width: 100%;
+    margin: 10px 0;
+    padding: 8px;
+    font-size: 1em;
+  }
+`;
+
 export class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleAttributeChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
-    return div('login');
+    const { username, password } = this.state;
+    return h(InputContainer, [
+      input({
+        id: 'username',
+        type: 'text',
+        name: 'username',
+        placeholder: 'username',
+        value: username,
+        onChange: this.handleAttributeChange.bind(this),
+      }),
+      input({
+        id: 'password',
+        type: 'password',
+        name: 'password',
+        placeholder: 'password',
+        value: password,
+        onChange: this.handleAttributeChange.bind(this),
+      }),
+      h(
+        Button,
+        {
+          onClick: () => console.log(username),
+        },
+        ['Login'],
+      ),
+    ]);
   }
 }
 
