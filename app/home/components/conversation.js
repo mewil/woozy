e304009678fa4e175a976ea77fee765079214fff
Shameek Ray <shameek@shameeks-mbp.lan>
2020-04-09 22:44:39 -0400

    contact conversation home-page and message components

--- app/home/components/conversation.js
+++ app/home/components/conversation.js
@@ -1,12 +1,8 @@
 import { Component } from 'react';
-import { h, div, li, ul } from 'react-hyperscript-helpers';
-import { connect } from 'react-redux';
+import { h, div } from 'react-hyperscript-helpers';
 import styled from 'styled-components';
-import { Title, Body, BodyFaded, TextFaded } from 'app/ui/typography.js';
-import { Input } from 'app/ui/input.js';
-import { Button } from 'app/ui/button.js';
+import { Body, Input } from '@woozy/ui';
 import { Message } from './message';
-
 
 /*
     senderID: this.props.senderID,
@@ -17,25 +13,24 @@ import { Message } from './message';
     content: this.props.content
  */
 
- 
 const tempMessages = [
-    {
-        senderID: 1,
-        recipientID:0,
-        timestamp: "NOW",
-        content: "Hey this is my first text.",
-        requestApproval: false,
-        status: "APPROVED"
-    },
-    {
-      senderID: 1,
-      recipientID:0,
-      timestamp: "NOW",
-      content: "Hey this is my second text.",
-      requestApproval: true,
-      status: "PENDING"
-  }
-]
+  {
+    senderID: 1,
+    recipientID: 0,
+    timestamp: 'NOW',
+    content: 'Hey this is my first text.',
+    requestApproval: false,
+    status: 'APPROVED',
+  },
+  {
+    senderID: 1,
+    recipientID: 0,
+    timestamp: 'NOW',
+    content: 'Hey this is my second text.',
+    requestApproval: true,
+    status: 'PENDING',
+  },
+];
 
 const ConversationHeaderWrapper = styled.div`
   width: 100%;
@@ -46,59 +41,54 @@ const ConversationHeaderWrapper = styled.div`
   outline-color: gray;
 `;
 
- export class Conversation extends Component { 
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        // contactName = this.props.contactName ? this.props.contactName === true : "Shameek Ray" 
-        selectedContactName: "Shameek Ray",
-        selectedContactID: 1,
-        isContactSelected: false,
-        isFriendContact: false,
-        isAvoidedContact: false
-      }
-    }
-  
-    contactHeadline() {
-      return (
-        h(ConversationHeaderWrapper, [
-          h(Body, this.state.contactName ? this.isContactSelected == true : "Choose a contact to see their messages."),
-          h(div, this.contactStatus())
-        ])
-      );
-    }
+export class Conversation extends Component {
+  constructor(props) {
+    super(props);
 
-  
-    contactStatus() {
-      if (this.state.isFriendContact) return "Woozy Friend"
-      else if (this.state.isAvoidedContact) return "Avoiding"
-      else return ""
-    }
-
-    sendNewText() {
-      return (
-        h(div, [
-          h(Input)]    
-        )
-      );
-    }
-
-    showMessages() {
-        return (
-            h(div, tempMessages.map(
-                message => h(Message, message)
-            ))
-        );
-    }
-
-    render() {
-      return (
-        h(div, [
-          this.contactHeadline(),
-          this.showMessages(),
-          this.sendNewText()
-        ])
-      );
-    }
+    this.state = {
+      // contactName = this.props.contactName ? this.props.contactName === true : "Shameek Ray"
+      selectedContactName: 'Shameek Ray',
+      selectedContactID: 1,
+      isContactSelected: false,
+      isFriendContact: false,
+      isAvoidedContact: false,
+    };
   }
+
+  contactHeadline() {
+    return h(ConversationHeaderWrapper, [
+      h(
+        Body,
+        this.state.contactName
+          ? this.isContactSelected === true
+          : 'Choose a contact to see their messages.',
+      ),
+      h(div, this.contactStatus()),
+    ]);
+  }
+
+  contactStatus() {
+    if (this.state.isFriendContact) return 'Woozy Friend';
+    else if (this.state.isAvoidedContact) return 'Avoiding';
+    return '';
+  }
+
+  sendNewText() {
+    return h(div, [h(Input)]);
+  }
+
+  showMessages() {
+    return h(
+      div,
+      tempMessages.map((message) => h(Message, message)),
+    );
+  }
+
+  render() {
+    return h(div, [
+      this.contactHeadline(),
+      this.showMessages(),
+      this.sendNewText(),
+    ]);
+  }
+}
