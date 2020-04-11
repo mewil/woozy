b499b3940dfcfba1660325e8d52cc5d15ea4361b
Sahil Patel <sletap@umich.edu>
2020-04-11 14:53:54 -0500

    run prettier

--- server/routes/api/message.js
+++ server/routes/api/message.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = express.Router();
 
-let Conversation = require('../../db/conversation');
-let Message = require('../../db/message');
-let User = require('../../db/user');
+const Conversation = require('../../db/conversation');
+const Message = require('../../db/message');
+const User = require('../../db/user');
 // const port = 3000;
 
-
 // Get all-messages
-routes.route('/all-messages').get(function(req, res) {
-    Message.find(function (err, post) {
-        if (err) return next(err);
-        res.json(post);
-    });
+routes.route('/all-messages').get((req, res) => {
+  Message.find((err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 // Get all messages for a conversation
-routes.route('/messages/:conversation').get(function(req, res) {
-    Message.find(req.params.conversation, function (err, post) {
-        if (err) return next(err);
-        res.json(post);
-    });
+routes.route('/messages/:conversation').get((req, res) => {
+  // reverse sort by created-by, give only last 25 messages
+  Message.find(req.params.conversation, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 // send a message Post request
-routes.route('/').post(function(req, res) {
-    let message = new Message(req.body);
-    message.save().then(message => {
-        res.status(200).json({'message': 'message added successfully'});
-    });
+routes.route('/').post((req, res) => {
+  const message = new Message(req.body);
+  message.save().then((message) => {
+    res.status(200).json({ message: 'message added successfully' });
+  });
 });
 // app.get('/', (req, res) => res.send('Hello World!'))
 
