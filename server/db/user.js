71e7d4b6c86c5baec00085c2a1a564d7fcdbfcbd
Michael Wilson <mewil@umich.edu>
2020-04-12 02:24:08 -0400

    Add POST user creation route

--- server/db/user.js
+++ server/db/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  user_id: mongoose.Schema.Types.ObjectId,
-  password: String,
-  trusted_friend_id: mongoose.Schema.Types.ObjectId,
-  avoiding_id: [mongoose.Schema.Types.ObjectId],
+  userId: mongoose.Schema.Types.ObjectId,
+  userName: String,
+  trustedFriendId: mongoose.Schema.Types.ObjectId,
+  avoidingId: [mongoose.Schema.Types.ObjectId],
 });
 
 module.exports = mongoose.model('User', userSchema);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const userRouter = require('./api/user');
 const messageRouter = require('./api/message');
 
+router.use('/user', userRouter);
 router.use('/message', messageRouter);
 
 module.exports = router;
