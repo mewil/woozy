2c910605f4af1d86b76a12932ee827878359434b
Aditi Krishnan <39627804+aditikr123@users.noreply.github.com>
2020-04-12 01:02:01 -0400

    Combined settings screens (#29)
    Co-authored-by: michael wilson <mewil@umich.edu>

--- app/settings/components/schedule-setup.js
+++ app/settings/components/schedule-setup.js
@@ -1,6 +1,11 @@
 import { Component } from 'react';
 import { h } from 'react-hyperscript-helpers';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+// import { theme } from '@woozy/theme';
+import { routes } from '../../app/constants';
+
+import { Calendar, Time } from '@woozy/ui';
 
 const OuterContainer = styled.div`
   display: flex;
@@ -14,22 +19,6 @@ const Title = styled.h1`
   align-items: center;
 `;
 
-const Rows = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 20px;
-`;
-
-const Select = styled.select`
-  align-items: center;
-  padding: 20px;
-`;
-
-const Option = styled.option`
-  color: black;
-`;
-
 const SmallHeader = styled.h4`
   font-size: 25px;
   align-items: center;
@@ -41,17 +30,26 @@ const SmallHeader = styled.h4`
 const Container = styled.button`
   display: flex;
   margin: 20px;
-  width: 350px;
-  flex-direction: row;
-  padding: 50px;
+  width: 200px;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.success};
+  padding: 10px;
   align-items: center;
+  height: 40px;
 `;
 
-const SaveButton = styled(Container)`
-  width: 200px;
-  background-color: green;
-  padding: 10px;
-  height: 40px;
+const StyledNavLink = styled(NavLink)`
+  color: ${({ theme }) => theme.primary};
+  font-size: 16px;
+  padding: 2px 20px;
+  margin: 10px 0 10px 15px;
+  border-radius: 5px;
+  text-decoration: none;
+  transition: all 0.3s;
+  &:first-child {
+    margin: 0;
+    margin-left: 15px;
+  }
 `;
 
 export class SchedulePage extends Component {
@@ -59,104 +57,12 @@ export class SchedulePage extends Component {
     return h(OuterContainer, [
       h(Title, 'When will you be going out?'),
       h(SmallHeader, 'Date'),
-      h(Rows, [
-        h(Select, [
-          h(Option, 'January'),
-          h(Option, 'February'),
-          h(Option, 'March'),
-          h(Option, 'April'),
-          h(Option, 'May'),
-          h(Option, 'June'),
-          h(Option, 'July'),
-          h(Option, 'August'),
-          h(Option, 'September'),
-          h(Option, 'October'),
-          h(Option, 'November'),
-          h(Option, 'December'),
-        ]),
-        h(Select, [
-          h(Option, 1),
-          h(Option, 2),
-          h(Option, 3),
-          h(Option, 4),
-          h(Option, 5),
-          h(Option, 6),
-          h(Option, 7),
-          h(Option, 8),
-          h(Option, 9),
-          h(Option, 10),
-          h(Option, 11),
-          h(Option, 12),
-          h(Option, 13),
-          h(Option, 14),
-          h(Option, 15),
-          h(Option, 16),
-          h(Option, 17),
-          h(Option, 18),
-          h(Option, 19),
-          h(Option, 20),
-          h(Option, 21),
-          h(Option, 22),
-          h(Option, 23),
-          h(Option, 24),
-          h(Option, 25),
-          h(Option, 26),
-          h(Option, 27),
-          h(Option, 28),
-          h(Option, 29),
-          h(Option, 30),
-          h(Option, 31),
-        ]),
-      ]),
+      h(Calendar, ''),
       h(SmallHeader, 'Start time'),
-      h(Rows, [
-        h(Select, [
-          h(Option, 1),
-          h(Option, 2),
-          h(Option, 3),
-          h(Option, 4),
-          h(Option, 5),
-          h(Option, 6),
-          h(Option, 7),
-          h(Option, 8),
-          h(Option, 9),
-          h(Option, 10),
-          h(Option, 11),
-          h(Option, 12),
-        ]),
-        h(Select, [
-          h(Option, '00'),
-          h(Option, '15'),
-          h(Option, '30'),
-          h(Option, '45'),
-        ]),
-        h(Select, [h(Option, 'am'), h(Option, 'pm')]),
-      ]),
+      h(Time, ''),
       h(SmallHeader, 'End time'),
-      h(Rows, [
-        h(Select, [
-          h(Option, 1),
-          h(Option, 2),
-          h(Option, 3),
-          h(Option, 4),
-          h(Option, 5),
-          h(Option, 6),
-          h(Option, 7),
-          h(Option, 8),
-          h(Option, 9),
-          h(Option, 10),
-          h(Option, 11),
-          h(Option, 12),
-        ]),
-        h(Select, [
-          h(Option, '00'),
-          h(Option, '15'),
-          h(Option, '30'),
-          h(Option, '45'),
-        ]),
-        h(Select, [h(Option, 'am'), h(Option, 'pm')]),
-      ]),
-      h(SaveButton, 'Done'),
+      h(Time, ''),
+      h(StyledNavLink, { to: routes.SETTINGS }, [h(Container, 'Done')]),
     ]);
   }
 }
