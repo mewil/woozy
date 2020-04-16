f3622a60262db4a0e60b167a944bcbe584a60a0d
Shameek Ray <shameek@umich.edu>
2020-04-16 16:48:56 -0400

    Rendering contacts conditionally in the friends and avoided contacts pages (#47)
    Co-authored-by: aditikr <aditikr@umich.edu>
    Co-authored-by: Shameek Ray <shameek@shameeks-mbp.lan>
    Co-authored-by: Michael Wilson <mewil@umich.edu>

--- app/user/selectors.js
+++ app/user/selectors.js
@@ -1,4 +1,4 @@
-import { get, isNull, filter } from 'lodash';
+import { get, isNull, filter, find } from 'lodash';
 
 export const getUsers = (state) => get(state, 'users', {});
 
@@ -8,3 +8,6 @@ export const getAuthUserIsLoggedIn = (state) => !isNull(getAuthUserId(state));
 
 export const getNotLoggedInUsers = (state) =>
   filter(getUsers(state), ({ id }) => id !== getAuthUserId(state));
+
+export const getLoggedInUser = (state) =>
+  find(getUsers(state), ({ id }) => id === getAuthUserId(state));
