6b0e61c9958fe6e0f4a11cebc1216d41261cc036
Rosawill <rosawill@umich.edu>
2020-04-16 16:41:04 -0400

    Put request for updating contacts (#46)
    Co-authored-by: Michael Wilson <mewil@umich.edu>

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -54,4 +54,40 @@ router.get('/', (_req, res) => {
   });
 });
 
+// POST (PUT) request for added an avoided contact
+router.put('/:id', (req, res) => {
+  const { username } = req.body;
+  if (!username) {
+    res.status(400).send({
+      status: false,
+      message: 'must include username JSON field',
+    });
+  } else {
+    User.findOneAndUpdate(
+      { username },
+      { username },
+      {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+      },
+      (err, user) => {
+        if (err) {
+          res.status(500).send({
+            status: false,
+            message: err,
+          });
+        } else {
+          user.avoidingId.push(req.params.id);
+          res.send({
+            status: true,
+            message: 'successfully updated avoided contacts',
+            data: user.avoidingId,
+          });
+        }
+      },
+    );
+  }
+});
+
 module.exports = router;
